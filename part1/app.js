329362bea42db21bf230ccefec7373339eb32aab
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-20 06:58:36 +0000

    [automatic commit] 2025-06-20 06:58:36 UTC

--- part1/app.js
+++ part1/app.js
@@ -11,13 +11,13 @@ async function insertSampleData() {
 
     // Insert users
     await conn.query(`
-      INSERT INTO Users (username, email, password_hash, role)
-    VALUES
-        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        ('rinikidas', 'riniki@example.com', 'hashed888', 'walker'),
-        ('karisamahroukh', 'karisa@example.com', 'hashed555', 'owner');
+        INSERT INTO Users (username, email, password_hash, role)
+        VALUES
+            ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+            ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+            ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+            ('rinikidas', 'riniki@example.com', 'hashed888', 'walker'),
+            ('karisamahroukh', 'karisa@example.com', 'hashed555', 'owner');
     `);
 
     // Insert dogs
