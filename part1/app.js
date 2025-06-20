2024c8e5fc8e3916c2a2869e9685917d67f30a34
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-20 07:09:34 +0000

    [automatic commit] 2025-06-20 07:09:34 UTC

--- part1/app.js
+++ part1/app.js
@@ -67,11 +67,11 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
     const [rows] = await pool.query(`
-      SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, u.username AS owner_username
+      SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
       FROM WalkRequests
       JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
       JOIN Users ON Dogs.owner_id = Users.user_id
-      WHERE wr.status = 'open'
+      WHERE WalkRequests.status = 'open'
     `);
     res.json(rows);
   } catch (err) {
