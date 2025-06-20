e56b1f922f7b60ce8cf7221872d9c10f53c9673d
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-20 10:33:33 +0000

    [automatic commit] 2025-06-20 10:33:32 UTC

--- part2/app.js
+++ part2/app.js
@@ -12,6 +12,14 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+const session = require('express-session');
+app.use(session({
+  secret: 'superdogsecret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
