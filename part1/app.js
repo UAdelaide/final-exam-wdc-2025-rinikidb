7c5c58a894aad8d99e5177772a2ab655d5f18688
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-20 06:59:38 +0000

    [automatic commit] 2025-06-20 06:59:38 UTC

--- part1/app.js
+++ part1/app.js
@@ -22,10 +22,13 @@ async function insertSampleData() {
 
     // Insert dogs
     await conn.query(`
-      INSERT IGNORE INTO Dogs (owner_id, name, size)
-      VALUES
-        ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
-        ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small')
+        INSERT INTO Dogs (owner_id, name, size)
+        VALUES
+            ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+            ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+            ((SELECT user_id FROM Users WHERE username = 'rinikidas'), 'Laika', 'medium'),
+            ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Joey', 'small'),
+            ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Roxy', 'medium');
     `);
 
     // Insert walk requests
