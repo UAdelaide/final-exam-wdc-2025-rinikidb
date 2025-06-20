27af252bd67b7f0cb451f12a83488f92f1edeb93
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-20 10:15:30 +0000

    [automatic commit] 2025-06-20 10:15:30 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,13 +37,13 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE username = ? AND password_hash = ?
-    `, [username, password]);
+      WHERE email = ? AND password_hash = ?
+    `, [email, password]);
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
