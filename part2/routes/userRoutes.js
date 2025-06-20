aad9a7b14ef809f5557c6570ebf9edb7b2fde45b
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-20 09:37:46 +0000

    [automatic commit] 2025-06-20 09:37:46 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,6 +49,14 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+     // Store user in session
+    req.session.user = {
+      id: rows[0].user_id,
+      username: rows[0].username,
+      role: rows[0].role
+    };
+
+
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
