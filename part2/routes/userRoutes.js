e0ab99882cf7c2bf58fbdfce83af6e09cdd33187
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-20 11:24:28 +0000

    [automatic commit] 2025-06-20 11:24:28 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -40,7 +40,8 @@ router.post('/login', async (req, res) => {
   const { username, password } = req.body;
   // console.log("Login attempt:", username, password);
 
-  // in the try block I am now using 
+  /* in the try block I am now using username as the way to determine the user and not email
+*/
   try {
     const [rows] = await db.query(`
       SELECT user_id, role FROM Users
