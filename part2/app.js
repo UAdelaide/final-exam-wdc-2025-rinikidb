b46c3cc33afc6f6129d7ae09ab4bb40f55b55889
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-20 10:07:09 +0000

    [automatic commit] 2025-06-20 10:07:09 UTC

--- part2/app.js
+++ part2/app.js
@@ -19,12 +19,5 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-// q13
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false
-}));
-
 // Export the app instead of listening here
 module.exports = app;
--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,13 +49,6 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-     // q13
-    req.session.user = {
-      id: rows[0].user_id,
-      username: rows[0].username,
-      role: rows[0].role
-    };
-
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
