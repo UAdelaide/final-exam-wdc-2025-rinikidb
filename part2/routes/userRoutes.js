dd577aac8bb8d0fedecc2ef9e2ed555cb0e9c6f1
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-20 10:41:54 +0000

    [automatic commit] 2025-06-20 10:41:54 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -38,6 +38,7 @@ router.get('/me', (req, res) => {
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
+  
 
   try {
     const [rows] = await db.query(`
