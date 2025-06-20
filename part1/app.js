f2573d19412281a0e4fd0fc3d85106b8d52f3eaa
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-20 07:14:42 +0000

    [automatic commit] 2025-06-20 07:14:42 UTC

--- part1/app.js
+++ part1/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('./db');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 // Insert sample records on startup
 async function insertSampleData() {
