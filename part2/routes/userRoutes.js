17ffc5abc7cf6dd9e743a58918da20951b6b8f51
rinikidb <riniki.dasbarman@student.adelaide.edu.au>
2025-06-21 03:03:31 +0000

    [automatic commit] 2025-06-21 03:03:31 UTC 103.232.160.43

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -60,7 +60,6 @@ router.post('/login', async (req, res) => {
       role: rows[0].role
     };
 
-
     res.json({ message: 'Login successful', user: req.session.user });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
